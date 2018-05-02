96c246ef875a53d79fddba9829afd61ed024c5d2
David C. Conner <david.conner@cnu.edu>
2018-05-02 12:03:18 -0400

    modify manipulation states

--- flexbe_manipulation_states/src/flexbe_manipulation_states/get_joint_values_state.py
+++ flexbe_manipulation_states/src/flexbe_manipulation_states/get_joint_values_state.py
@@ -15,30 +15,40 @@ Created on 06.03.2016
 class GetJointValuesState(EventState):
 	'''
 	Retrieves current values of specified joints.
+	Joints are specified by parameter list.
 
-	-- joints		string[]	List of desired joint names.
+	-- joints				string[]	List of desired joint names.
+	-- timeout				double		Timeout value (optional)
+	-- joint_states_topic	string		Optional name of joint states topic
+										(default: /joint_states)
 
 	#> joint_values float[] 	List of current joint values.
 
 	<= retrieved 				Joint values are available.
+	<= timeout	 				Joint values are not available.
 
 	'''
 
-	def __init__(self, joints):
+	def __init__(self, joints, timeout=None, joint_states_topic='/joint_states'):
 		'''
 		Constructor
 		'''
 		super(GetJointValuesState, self).__init__(outcomes=['retrieved'],
 												output_keys=['joint_values'])
-		
-		self._topic = '/joint_states'
+
+		self._topic = joint_states_topic
 		self._sub = ProxySubscriberCached({self._topic: JointState})
 
 		self._joints = joints
 		self._joint_values = list()
-			
-		
+		self._timeout = timeout
+
+
 	def execute(self, userdata):
+		if (self._return_code is not None):
+			# Handle blocked transition or error during on_enter
+			return self._return_code
+
 		while self._sub.has_buffered(self._topic):
 			msg = self._sub.get_from_buffer(self._topic)
 			for i in range(len(msg.name)):
@@ -48,12 +58,20 @@ class GetJointValuesState(EventState):
 
 		if all(v is not None for v in self._joint_values):
 			userdata.joint_values = self._joint_values
+			self._return_code = 'retrieved'
 			return 'retrieved'
 
-			
+		if (self._timeout is not None and \
+			(Time.now()-self._start_time) > rospy.Duration(self._timeout) ):
+			self._return_code = 'timeout'
+			return 'timeout'
+
 	def on_enter(self, userdata):
 		self._sub.enable_buffer(self._topic)
 		self._joint_values = [None] * len(self._joints)
+		self._return_code = None
+		self._start_time = rospy.Time.now()
+		userdata.joint_values = None
 
 	def on_exit(self, userdata):
 		self._sub.disable_buffer(self._topic)
