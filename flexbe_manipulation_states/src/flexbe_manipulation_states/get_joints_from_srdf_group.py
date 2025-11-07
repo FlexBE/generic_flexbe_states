2b29d17216a8f80cd90aa62653b9c70d40ed87be
dcconner <david.conner@cnu.edu>
2025-11-07 18:08:07 -0500

    modify manipulation states to add timeout and other parameters  (#11)
    * modify manipulation states
    
    * add most basic state import tests
    
    * add timeout outcome to constructor

--- flexbe_manipulation_states/src/flexbe_manipulation_states/get_joints_from_srdf_group.py
+++ flexbe_manipulation_states/src/flexbe_manipulation_states/get_joints_from_srdf_group.py
@@ -14,7 +14,8 @@ Created on 18.06.2016
 
 class GetJointsFromSrdfGroup(EventState):
 	'''
-	Simple state to look up a pre-defined joint configuration from the given joint group in a SRDF file.
+	Simple state to look up a pre-defined list of joint names from a given
+	joint group defined in a SRDF file.
 	This state is recommended if you only need these values without any unnecessary overhead.
 
 	-- move_group 	string 		Name of the move group of interest.
@@ -22,6 +23,8 @@ class GetJointsFromSrdfGroup(EventState):
 	-- robot_name 	string 		Optional name of the robot to be used.
 								If left empty, the first one found will be used
 								(only required if multiple robots are specified in the same file).
+	-- srdf_name	string		Optional name of the srdf parameter
+								(default: "/robot_description_semantic")
 
 	#> joint_names string[] 	List of joint values for the requested group.
 
@@ -30,30 +33,32 @@ class GetJointsFromSrdfGroup(EventState):
 
 	'''
 
-	def __init__(self, move_group, robot_name=""):
+	def __init__(self, move_group, robot_name="", srdf_name = "/robot_description_semantic"):
 		'''
 		Constructor
 		'''
 		super(GetJointsFromSrdfGroup, self).__init__(outcomes=['retrieved', 'param_error'],
-												output_keys=['joint_names'])
+														output_keys=['joint_names'])
 
 		self._move_group = move_group
 		self._robot_name = robot_name
+		self._srdf_name  = srdf_name
 
-		# Check existence of SRDF parameter.
-		# Anyways, values will only be read during runtime to allow modifications.
 		self._srdf_param = None
-		if rospy.has_param("/robot_description_semantic"):
-			self._srdf_param = rospy.get_param("/robot_description_semantic")
+		if rospy.has_param(self._srdf_name):
+			self._srdf_param = rospy.get_param(self._srdf_name)
 		else:
-			Logger.logerr('Unable to get parameter: /robot_description_semantic')
+			Logger.logerr('Unable to get parameter: ' + self._srdf_name)
 
 		self._file_error = False
 		self._srdf = None
+		self._return_code = None
 
-		
 	def execute(self, userdata):
 
+		if (self._return_code is not None):
+			# Handle blocked transition or error during on_enter
+			return self._return_code
 
 		robot = None
 		for r in self._srdf.iter('robot'):
@@ -62,6 +67,7 @@ class GetJointsFromSrdfGroup(EventState):
 				break
 		if robot is None:
 			Logger.logwarn('Did not find robot name in SRDF: %s' % self._robot_name)
+			self._return_code = 'param_error'
 			return 'param_error'
 
 		group = None
@@ -71,25 +77,33 @@ class GetJointsFromSrdfGroup(EventState):
 				break
 		if group is None:
 			Logger.logwarn('Did not find group name in SRDF: %s' % self._move_group)
+			self._return_code = 'param_error'
 			return 'param_error'
 
 		try:
 			userdata.joint_names = [str(j.attrib['name']) for j in group.iter('joint')]
 		except Exception as e:
 			Logger.logwarn('Unable to parse joint values from SRDF:\n%s' % str(e))
+			self._return_code = 'param_error'
 			return 'param_error'
 
+		self._return_code = 'retrieved'
 		return 'retrieved'
 
-			
+
 	def on_enter(self, userdata):
-		# Parameter check
-		if self._srdf_param is None:
-			self._param_error = True
+		# Check existence of SRDF parameter.
+		# Values are read during runtime to allow modifications.
+		if rospy.has_param(self._srdf_name):
+			self._srdf_param = rospy.get_param(self._srdf_name)
+		else:
+			Logger.logerror("Unable to get parameter: "+self._srdf_name)
+			self._return_code = 'param_error'
 			return
 
+		# Parse the SRDF string
 		try:
 			self._srdf = ET.fromstring(self._srdf_param)
 		except Exception as e:
 			Logger.logwarn('Unable to parse given SRDF parameter: /robot_description_semantic')
-			self._param_error = True
+			self._return_code = 'param_error'
